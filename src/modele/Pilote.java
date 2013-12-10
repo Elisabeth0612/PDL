71076443113db1cebcc62fc57f5b7cc405df3d0f
Elisabeth <elisabeth.recule@hotmail.fr>
2013-12-10 22:42:29 +0100

    javadoc Pilote

--- src/modele/Pilote.java
+++ src/modele/Pilote.java
@@ -2,41 +2,80 @@ package modele;
 
 import java.util.List;
 
-
+/**
+ * @author Coralie GENDRIER - Corinne BECHEPOIS - Elisabeth RECULE - M1 MIAGE
+ * grpe 1A
+ */
 public class Pilote {
 
-	private String nom, prenom, couleursCasque;
-	
-	public Pilote(String nom, String prenom, String couleursCasque){
-		this.nom=nom;
-		this.prenom=prenom;
-		this.couleursCasque=couleursCasque;
-	}
+    private String nom, prenom, couleursCasque;
 
-	public void removePilote(List<Pilote> listP){
-		listP.remove(this);
-	}
-	public String getNom() {
-		return nom;
-	}
+    /**
+     * Instanciation de Pilote
+     * 
+     * @param nom
+     * @param prenom
+     * @param couleursCasque
+     */
+    public Pilote(String nom, String prenom, String couleursCasque) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.couleursCasque = couleursCasque;
+    }
 
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
+    /**
+     * Méthode permettant de supprimer le pilote actuel d'une liste passée en paramètre
+     * @param listP Liste de Pilote
+     */
+    public void removePilote(List<Pilote> listP) {
+        listP.remove(this);
+    }
 
-	public String getPrenom() {
-		return prenom;
-	}
+    /**
+     * Méthode permettant de récupérer le nom du pilote
+     * @return String - nom
+     */
+    public String getNom() {
+        return nom;
+    }
 
-	public void setPrenom(String prenom) {
-		this.prenom = prenom;
-	}
+    /**
+     * Méthode permettant de modifier le nom du pilote
+     * @param nom String
+     */
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
 
-	public String getCouleursCasque() {
-		return couleursCasque;
-	}
+    /**
+     * Méthode permettant de récupérer le prénom du pilote
+     * @return String - prénom
+     */
+    public String getPrenom() {
+        return prenom;
+    }
 
-	public void setCouleursCasque(String couleursCasque) {
-		this.couleursCasque = couleursCasque;
-	}
+    /**
+     * Méthode permettant de modifier le prénom du pilote
+     * @param prenom String
+     */
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    /**
+     * Méthode permettant de récupérer la couleur du casque du pilote
+     * @return String - couleur du casque
+     */
+    public String getCouleursCasque() {
+        return couleursCasque;
+    }
+
+    /**
+     * Méthode permettant de modifier la couleur du casque du pilote
+     * @param couleursCasque String
+     */
+    public void setCouleursCasque(String couleursCasque) {
+        this.couleursCasque = couleursCasque;
+    }
 }
